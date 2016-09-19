7dd72b17118179a945acd518fd415b233041a21b
arnav dhamija <arnav.dhamija@gmail.com>
2016-09-20 00:58:07 +0530

    Adding history buffer

--- visualiser.cpp
+++ visualiser.cpp
@@ -7,7 +7,7 @@
 #include <ctime>
 #include <time.h>
 //#include <cstdio.h>
-const int bands = 256;
+const int bands = 64;
 const int n = 256;
 const int width = n / bands;
 int main()
@@ -18,35 +18,49 @@ int main()
     int i;
     t.setSize(sf::Vector2f(200, 500));
     t.setPosition(500, 100);
-    std::vector<double> val(256);
+    std::vector<double> val(n);
     std::vector<double> subbands(bands);
+    std::vector<double> history(n*43);
     std::ifstream input("output/fftWindows");
 //    sf::Color barColor;
     for (i = 0; i < bands; i++) {
         bars[i].setFillColor(sf::Color(n - i * width, 0 + i * width, i*width));
     }
     sf::Music music;
-    if (!music.openFromFile("sounds/file3.wav")) {
+    if (!music.openFromFile("sounds/file2.wav")) {
         return -1;
     }
     music.play();
+    auto historyIt = history.begin();
+    while (historyIt != history.end()) {
+        *historyIt=0;
+        historyIt++;
+    }
+    input.close();
+    input.open("output/fftWindows");
     while (window.isOpen()) {
         //while (!input.eof()) {
         std::clock_t start = std::clock();
         double a, b;
         for (i = 0; i < n && !input.eof(); i++) {
             input >> a >> b;
-
             input >> val[i];
             //input.flush();
         }
-        std::cout << a << std::endl;
+        for (i = 0; i < 43 * (n - 1); i++) {
+			history[n + i] = history[i];
+		}
+		for (i = 0; i < n; i++) {
+			history[i] = val[i];
+		}
+
         for (i = 0; i < bands; i++) {
             double sum=0;
             for (int j = 0; j < width; j++) {
                 sum+=val[j+i*width];
             }
             subbands[i] = sum / width;
+            std::cout << a << ' ' << subbands[i] << std::endl;
         }
         for (i = 0; i < bands; i++) {
             double height = subbands[i] *100;
